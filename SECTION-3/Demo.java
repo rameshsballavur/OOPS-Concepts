dad9c9cbdbc280d511a177c641fe003ca0a763b5
Ramesh S Ballavur <66952707+rameshsballavur@users.noreply.github.com>
2022-04-02 16:22:26 +0530

    Custom Exception

---
+++ SECTION-3/Demo.java
@@ -0,0 +1,29 @@
+package com.java.numbers;
+
+class InvalidAgeException extends Exception{
+	public InvalidAgeException(String str) {
+		super(str);
+	}
+}
+
+public class Demo {
+	public static void validate(int age) throws InvalidAgeException{
+		if(age<18) {
+			throw new InvalidAgeException("Age is not valid to vote");
+		}
+		else {
+			System.out.println("welcome to voting");
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			validate(23);
+			System.out.println("=========================");
+			validate(13);
+		} catch (InvalidAgeException e) {
+			System.out.println("Exception occured "+e);
+		}
+		System.out.println("Rest of the code......");
+	}
+}
